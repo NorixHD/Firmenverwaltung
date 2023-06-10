909f4236cd8dd5751f39c22240abdc4a823f2225
informatikRocks <132548073+informatikRocks@users.noreply.github.com>
2023-06-10 14:46:00 +0200

    Update Gui.java
    Ich habe meine Veränderungen eingefügt

--- Programm/src/Gui.java
+++ Programm/src/Gui.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
@@ -19,6 +20,7 @@ import javax.swing.JList;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import java.awt.SystemColor;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
