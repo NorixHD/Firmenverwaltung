d7d9b03b06e4f362fe707b8432015a81f046542e
michael-pannier <132549005+michael-pannier@users.noreply.github.com>
2023-06-27 11:15:51 +0200

    Add files via upload

---
+++ Programm/src/AuftragsverwaltungGUI.java
@@ -0,0 +1,129 @@
+package de.pannier.firmenverwaltung;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+
+public class AuftragsverwaltungGUI {
+    private static String DATEI_NAME;
+
+    private JFrame frame;
+    private JTextField datumTextField;
+    //private JTextField auftragsnummerTextField;
+    private JTextField anschriftTextField;
+    private JTextArea beschreibungTextArea;
+    private static final String DATEINAME = "variablen.txt";
+
+    public static void main(String[] args) {
+        AuftragsverwaltungGUI auftragsverwaltung = new AuftragsverwaltungGUI();
+        auftragsverwaltung.start();
+        
+    }
+
+    public void start() {
+        frame = new JFrame("Auftragsverwaltung");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel datumLabel = new JLabel("Datum: (dd.mm.yyyy)");
+        datumTextField = new JTextField(20);
+        //JLabel auftragsnummerLabel = new JLabel("Auftragsnummer:");
+        //auftragsnummerTextField = new JTextField(20);
+        JLabel anschriftLabel = new JLabel("Anschrift: (Straße, Hausnummer, Ort, Postleitzahl)");
+        anschriftTextField = new JTextField(20);
+        JLabel beschreibungLabel = new JLabel("Beschreibung:");
+        beschreibungTextArea = new JTextArea(5, 20);
+        JScrollPane beschreibungScrollPane = new JScrollPane(beschreibungTextArea);
+        JButton auftragHinzufuegenButton = new JButton("Auftrag hinzufügen");
+        auftragHinzufuegenButton.addActionListener(new AuftragHinzufuegenListener());
+
+        panel.add(datumLabel);
+        panel.add(datumTextField);
+        //panel.add(auftragsnummerLabel);
+        //panel.add(auftragsnummerTextField);
+        panel.add(anschriftLabel);
+        panel.add(anschriftTextField);
+        panel.add(beschreibungLabel);
+        panel.add(beschreibungScrollPane);
+        panel.add(auftragHinzufuegenButton);
+
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+        
+        
+    }
+
+    private void auftragHinzufuegen() {
+        String datum = datumTextField.getText();
+        //String auftragsnummer = auftragsnummerTextField.getText();
+        String anschrift = anschriftTextField.getText();
+        String beschreibung = beschreibungTextArea.getText();
+
+        try {
+            DATEI_NAME = "Auftrag" + auslesen() + ".txt";
+        	BufferedWriter writer = new BufferedWriter(new FileWriter(DATEI_NAME, true));
+            writer.write("Datum: " + datum);
+            writer.newLine();
+            writer.write("Auftragsnummer: " + auslesen());
+            writer.newLine();
+            writer.write("Anschrift: " + anschrift);
+            writer.newLine();
+            writer.newLine();
+            writer.write("Beschreibung: ");
+            writer.newLine();
+            writer.write(beschreibung);
+            writer.newLine();
+            writer.close();
+
+            datumTextField.setText("");
+            //auftragsnummerTextField.setText("");
+            anschriftTextField.setText("");
+            beschreibungTextArea.setText("");
+
+            JOptionPane.showMessageDialog(frame, "Auftrag wurde hinzugefügt.");
+            speichern(auslesen()+1);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void speichern(int zahl) {
+        try {
+            FileWriter writer = new FileWriter(DATEINAME);
+            writer.write(Integer.toString(zahl));
+            writer.close();
+            System.out.println("Variable erfolgreich in der Datei gespeichert.");
+        } catch (IOException e) {
+            System.out.println("Fehler beim Speichern der Variable: " + e.getMessage());
+        }
+    }
+
+    private static int auslesen() {
+        try {
+            Scanner scanner = new Scanner(new java.io.File(DATEINAME));
+            int gespeicherteZahl = scanner.nextInt();
+            scanner.close();
+            return gespeicherteZahl;
+        } catch (IOException e) {
+            System.out.println("Fehler beim Lesen der Variable: " + e.getMessage());
+        }
+
+        return 0;
+    }
+
+    private class AuftragHinzufuegenListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            auftragHinzufuegen();
+        }
+    }
+}
---
+++ Programm/src/TextdateiAnzeiger.java
@@ -0,0 +1,73 @@
+package de.pannier.firmenverwaltung;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class TextdateiAnzeiger {
+    private JFrame frame;
+    private JTextArea textArea;
+
+    public static void main(String[] args) {
+        TextdateiAnzeiger anzeiger = new TextdateiAnzeiger();
+        anzeiger.start();
+    }
+
+    public void start() {
+        frame = new JFrame("Textdatei Anzeiger");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        // Textbereich für den Inhalt
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        // Button zum Laden der Datei
+        JButton ladenButton = new JButton("Datei laden");
+        ladenButton.addActionListener(new LadenButtonListener());
+
+        panel.add(scrollPane, BorderLayout.CENTER);
+        panel.add(ladenButton, BorderLayout.SOUTH);
+
+        frame.getContentPane().add(panel);
+        frame.setSize(600, 400);
+        frame.setVisible(true);
+    }
+
+    private void ladenDatei() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File("C:/Users/micha/eclipse-workspace/FirmenverwaltungTest"));
+        fileChooser.setFileFilter(new FileNameExtensionFilter("Textdateien", "txt"));
+        int result = fileChooser.showOpenDialog(frame);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            try {
+                StringBuilder content = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    content.append(line).append("\n");
+                }
+                reader.close();
+                textArea.setText(content.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    private class LadenButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            ladenDatei();
+        }
+    }
+}
